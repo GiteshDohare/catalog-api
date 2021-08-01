62ec471ff76e1d854977f9502c7d8b46bd0261ca
Gitesh <giteshkumar.dohare@gmail.com>
2021-08-02 02:34:06 +0530

    changed exception message case

--- src/main/java/com/omnirio/catalog/exception/AttributeNotFoundException.java
+++ src/main/java/com/omnirio/catalog/exception/AttributeNotFoundException.java
@@ -2,7 +2,7 @@ package com.omnirio.catalog.exception;
 
 public class AttributeNotFoundException extends RuntimeException {
     public AttributeNotFoundException(Long id) {
-        super("Invalid Attribute id: " + id);
+        super("Invalid attribute id: " + id);
     }
 
 }
--- src/main/java/com/omnirio/catalog/exception/ProductNotFoundException.java
+++ src/main/java/com/omnirio/catalog/exception/ProductNotFoundException.java
@@ -2,7 +2,7 @@ package com.omnirio.catalog.exception;
 
 public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(Long id) {
-        super("Invalid Product id: " + id);
+        super("Invalid product id: " + id);
     }
 
 }
