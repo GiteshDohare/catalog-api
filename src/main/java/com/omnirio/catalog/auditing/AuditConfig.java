289d70a6f4455f0333035c5b529fc053a550480e
Gitesh <giteshkumar.dohare@gmail.com>
2021-08-01 22:05:18 +0530

    Added Configs

---
+++ src/main/java/com/omnirio/catalog/auditing/AuditConfig.java
@@ -0,0 +1,10 @@
+package com.omnirio.catalog.auditing;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditConfig {
+}
